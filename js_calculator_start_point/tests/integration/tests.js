f8524f304c5ce5a62d46237f9963c325ed708563
Paul McManus <pmcmanus2021@gmail.com>
2020-01-24 15:13:10 +0000

    Passes update display

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -14,6 +14,17 @@ describe('calculator functionality', function() {
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  }),
+
+  it('should update display with the result of operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
   })
+
+
 
 });
